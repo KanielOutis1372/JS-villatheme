bab98314c3b2d2b4a9bd5b7ad2bfb4fdaeda8c3f
huy <90317325+KanielOutis1372@users.noreply.github.com>
2023-05-27 17:34:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var count = 0;
 
 function addFormInput() {
     count++;
-    console.log(count);
+    // console.log(count);
     var getWrapRight = document.getElementById('wrap-right');
     console.log(getWrapRight);
     const formBuild = document.createElement('div');
-    formBuild.id = 'form-build';
-    formBuild.classList.add(count);
+    formBuild.classList.add('form-build');
+    // formBuild.id = count;
     formBuild.innerHTML = `
         <div id="form-wrap">
             <div id="form-head">
@@ -19,9 +19,10 @@ function addFormInput() {
                 <div class="group type">
                     <label for="">Type</label>
                     <select name="" id="">
-                        <option value="text" id="option1">text</option>
-                        <option value="text" id="option2">textarea</option>
-                        <option value="button" id="option3">button</option>
+                        <option value="text">text</option>
+                        <option value="email">email</option>
+                        <option value="number">number</option>
+                        <option value="password">password</option>
                     </select>
                 </div>
 
@@ -59,11 +60,11 @@ function addFormInput() {
 
 function addFormTextarea() {
     count++;
-    console.log(count);
+    // console.log(count);
     var getWrapRight = document.getElementById('wrap-right');
     console.log(getWrapRight);
     const formBuild = document.createElement('div');
-    formBuild.id = 'form-build';
+    formBuild.classList.add('form-build');
     formBuild.innerHTML = `
         <div id="form-wrap">
             <div id="form-head">
@@ -74,43 +75,41 @@ function addFormTextarea() {
                 <div class="group">
                     <label for="">Type</label>
                     <select name="" id="">
-                        <option value="text">text</option>
-                        <option value="text">textarea</option>
-                        <option value="button">button</option>
+                        <option value="textarea">textarea</option>
                     </select>
                 </div>
 
-                <div class="group">
+                <div class="group label">
                     <label for="">Label</label>
                     <input type="text">
                 </div>
 
-                <div class="group">
+                <div class="group name">
                     <label for="">Name</label>
                     <input type="text">
                 </div>
 
-                <div class="group">
+                <div class="group id">
                     <label for="">Id</label>
                     <input type="text">
                 </div>
 
-                <div class="group">
+                <div class="group placeholder">
                     <label for="">Placeholder</label>
                     <input type="text">
                 </div>
 
-                <div class="group">
+                <div class="group row">
                 <label for="">Row</label>
-                <input type="number">
+                <input type="number" value="0" min=0>
                 </div>
 
-                <div class="group">
+                <div class="group column">
                     <label for="">Column</label>
-                    <input type="number">
+                    <input type="number" value="0" min=0>
                 </div>
 
-                <div class="group">
+                <div class="group require">
                     <label for="">Require</label>
                     <span>
                         <input type="checkbox">
@@ -124,11 +123,11 @@ function addFormTextarea() {
 
 function addFormButton() {
     count++;
-    console.log(count);
+    // console.log(count);
     var getWrapRight = document.getElementById('wrap-right');
     console.log(getWrapRight);
     const formBuild = document.createElement('div');
-    formBuild.id = 'form-build';
+    formBuild.classList.add('form-build');
     formBuild.innerHTML = `
     <div id="form-wrap">
         <div id="form-head">
@@ -139,33 +138,31 @@ function addFormButton() {
             <div class="group">
                 <label for="">Type</label>
                 <select name="" id="">
-                    <option value="text">text</option>
-                    <option value="text">texterea</option>
                     <option value="button">button</option>
                 </select>
             </div>
 
-            <div class="group">
+            <div class="group label">
                 <label for="">Label</label>
                 <input type="text">
             </div>
 
-            <div class="group">
+            <div class="group name">
                 <label for="">Name</label>
                 <input type="text">
             </div>
 
-            <div class="group">
+            <div class="group id">
                 <label for="">Id</label>
                 <input type="text">
             </div>
 
-            <div class="group">
+            <div class="group placeholder">
                 <label for="">Placeholder</label>
                 <input type="text">
             </div>
 
-            <div class="group">
+            <div class="group require">
                 <label for="">Require</label>
                 <span>
                     <input type="checkbox">
@@ -179,95 +176,140 @@ function addFormButton() {
 
 function removeForm() {
     count--;
-    console.log(count);
-    var formBuild = document.getElementById('form-build');
+    // console.log(count);
+    var formBuild = document.querySelector('.form-build');
     formBuild.parentElement.removeChild(formBuild);    
 }
-
-function getDataFormInputField() {
-    // const FormInputField = {}
-
-    // let selectedOptionText = '';
-
-    // const selectElement = document.querySelector('.group select');
-
-    //     // Add event listener to the <select> element
-    // selectElement.addEventListener('change', () => {
-    //     // Get the selected option
-    //     const selectedOption = selectElement.options[selectElement.selectedIndex];
-
-    //     // Get the value and text of the selected option
-    //     const selectedValue = selectedOption.value;
-    //     const selectedText = selectedOption.text;
-
-    //     // Do something with the selected value or text
-    //     console.log('Selected value:', selectedValue);
-    //     console.log('Selected text:', selectedText);
-    // });
-
-    // // You can access the selected option text outside the event listener
-    // console.log('Initial selected option text:', selectedOptionText);
-    // // console.log(a);
-
-    // const _label = document.querySelector('.label input').value;
-    // const _name = document.querySelector('.name input').value;
-    // const _id = document.querySelector('.id input').value;
-    // const _placeholder = document.querySelector('.placeholder input').value;
-    // const _require = document.querySelector('.require input').value;
-    // const FormInputField = new Object();
-    // FormInputField.label = _label;
-    // FormInputField.name = _name;
-    // FormInputField.id = _id;
-    // FormInputField.placeholder = _placeholder;
-    // FormInputField.require = _require;
-    // console.log(FormInputField);
-}
-
 
 const arr = [];
 
 function saveData() {
-    // const title = document.querySelector('#form-title').textContent;
-    // const _label = document.querySelector('.label input').value;
-    // const _name = document.querySelector('.name input').value;
-    // const _id = document.querySelector('.id input').value;
-    // const _placeholder = document.querySelector('.placeholder input').value;
-    // const _require = document.querySelector('.require input').value;
-    // const _row = document.querySelector('.row').value;
-    // const _column = document.querySelector('.column').value;
-    // // console.log(title);
-    // if (title == 'Input field') {
-    //     const FormInputField = new Object();
-    //     FormInputField.label = _label;
-    //     FormInputField.name = _name;
-    //     FormInputField.id = _id;
-    //     FormInputField.placeholder = _placeholder;
-    //     FormInputField.require = _require;
-    //     arr.push(FormInputField);
-    //     removeForm();
-    // } 
-    // else if (title == 'Textarea field') {
-    //     const FormTextareaField = new Object();
-    //     FormTextareaField.label = _label;
-    //     FormTextareaField.name = _name;
-    //     FormTextareaField.id = _id;
-    //     FormTextareaField.placeholder = _placeholder;
-    //     FormTextareaField.row = _row;
-    //     FormTextareaField.column = _column;
-    //     FormTextareaField.require = _require;
-    //     arr.push(FormTextareaField);
-    //     removeForm();
-    // } 
-    // else if (title == 'Button field') {
-    //     const FormButtonField = new Object();
-    //     FormButtonField.label = _label;
-    //     FormButtonField.name = _name;
-    //     FormButtonField.id = _id;
-    //     FormButtonField.placeholder = _placeholder;
-    //     FormButtonField.require = _require;
-    //     arr.push(FormButtonField);
-    //     removeForm();
-    // }
- 
-    // console.log(arr);
+    const list = document.querySelectorAll('.form-build');
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+        var formTitle = element.querySelector('#form-title').textContent;
+        if (formTitle == 'Input field') {
+            const inputForm = {};
+            inputForm.title = 'Input field';
+            inputForm.valueType = element.querySelector("select").value;
+            inputForm.valueLabel = element.querySelector('.label input').value;
+            inputForm.valueName = element.querySelector('.name input').value;
+            inputForm.valueId = element.querySelector('.id input').value;
+            inputForm.valuePlaceholder = element.querySelector('.placeholder input').value;
+            const require = element.querySelector('.require input');
+            const isChecked = require.checked;
+            if (isChecked) {
+                inputForm.valueRequire = true;
+            }
+            else {
+                inputForm.valueRequire = false;
+            }
+            arr.push(inputForm);
+        }
+        else if (formTitle == 'Texterea field') {
+            const textareaForm = {};
+            textareaForm.title = 'Textarea field';
+            textareaForm.valueType = element.querySelector("select").value;
+            textareaForm.valueLabel = element.querySelector('.label input').value;
+            textareaForm.valueName = element.querySelector('.name input').value;
+            textareaForm.valueId = element.querySelector('.id input').value;
+            textareaForm.valuePlaceholder = element.querySelector('.placeholder input').value;
+            textareaForm.valueRow = element.querySelector('.row input').value;
+            textareaForm.valueColumn = element.querySelector('.column input').value;
+            const require = element.querySelector('.require input');
+            const isChecked = require.checked;
+            if (isChecked) {
+                textareaForm.valueRequire = true;
+            }
+            else {
+                textareaForm.valueRequire = false;
+            }
+            arr.push(textareaForm);
+        }
+        else if (formTitle == 'Button field') {
+            const buttonForm = {};
+            buttonForm.title = 'Button field';
+            buttonForm.valueType = element.querySelector('select').value;
+            buttonForm.valueLabel = element.querySelector('.label input').value;
+            buttonForm.valueName = element.querySelector('.name input').value;
+            buttonForm.valueId = element.querySelector('.id input').value;
+            buttonForm.valuePlaceholder = element.querySelector('.placeholder input').value;
+            const require = element.querySelector('.require input');
+            const isChecked = require.checked;
+            if (isChecked) {
+                buttonForm.valueRequire = true;
+            }
+            else {
+                buttonForm.valueRequire = false;
+            }
+            arr.push(buttonForm);
+        }
+    }
+
+    const myArr = JSON.stringify(arr); 
+    if(localStorage.getItem('FORMSDATA')) {
+        localStorage.setItem('FORMSDATA', myArr);
+    }
+    else {
+        localStorage.setItem('FORMSDATA', myArr);
+    }
 }
+
+function setValueForForms(element, item) {
+    element.querySelector('select').value = item.valueType.trim();
+    element.querySelector('.label input').value = item.valueLabel.trim();
+    element.querySelector('.name input').value = item.valueName.trim();
+    element.querySelector('.id input').value = item.valueId.trim();
+    element.querySelector('.placeholder input').value = item.valuePlaceholder.trim();
+    const checkbox = item.valueRequire;
+    console.log(checkbox);
+    if (checkbox) {
+        element.querySelector('.require input').checked = true;
+    } 
+    else {
+        element.querySelector('.require input').checked = false;
+    }
+}
+
+function loadData() {
+    const array = localStorage.getItem('FORMSDATA');
+    const convertArrayy = JSON.parse(array);
+    
+    for (const item of convertArrayy) {
+        if (item.title == 'Input field') {
+            addFormInput();
+        }  
+        
+        if (item.title == 'Textarea field') {
+            addFormTextarea();
+        }
+        if (item.title == 'Button field') {
+            addFormButton();
+        }
+    }
+
+    const listForm = document.querySelectorAll('.form-build');
+    for (let i = 0; i < listForm.length; i++)  {
+        if (convertArrayy[i].title == 'Textarea field') {
+            listForm[i].querySelector('.row input').value = convertArrayy[i].valueRow;
+            listForm[i].querySelector('.column input').value = convertArrayy[i].valueColumn;
+        }
+        setValueForForms(listForm[i], convertArrayy[i]);
+    }
+}
+
+loadData();
+
+function validate(element) {
+    element.querySelector('.label input').value;
+}
+
+
+function isValidPhoneNumber(value) {
+// Sử    dụng biểu thức chính quy để kiểm tra định dạng số điện thoại (ví dụ: dạng XXX-XXXXXXX)
+    var phoneRegex = /^\d{3}-\d{7}$/;
+    return phoneRegex.test(value);
+}
+
+
+  
